e346ce5c959b6bd0f41cf261322b62ae81cb6e16
karanpatel45 <karan@gmail.com>
2023-03-16 21:21:48 -0700

    Max

--- text.asm
+++ text.asm
@@ -20,23 +20,26 @@ main:
 	move $t1, $s0
 	lw $t2, 0($t0)		#loads the 1st element of an array
 	
-	loop:
-		#If the size of an array is 0 exit.
-		beq $t1, $zero, end
-		addi $t0, $t0, 4	#next element of the array
-		lw $t3, ($t0)		#load the current element
-		beq $t3, $zero, end  # check if the end of the array is reached
-		bge $t2, $t3, greater
-		move $t2, $t3
-  
-		# decrement length of array
-		addi $t1, $t1, -1
-		j loop
-	greater:
-		addi $t1, $t1, -1
-		bne $t1, $zero, loop
+getMax:
+	#If the size of an array is 0 exit.
+	beq $t1, $zero, end
+	addi $t0, $t0, 4	#next element of the array
+	lw $t3, ($t0)		#load the current element
+	beq $t3, $zero, end  # check if the end of the array is reached
+	bge $t2, $t3, greater
+	move $t2, $t3		#Updates the max
+ 
+	# decrement length of array
+	addi $t1, $t1, -1
+	j getMax
+greater:
+	addi $t1, $t1, -1
+	bne $t1, $zero, getMax
 		
-	#Exits the Program
-	end:	
-	li $v0, 10
-	syscall
+#Exits the Program
+end:	
+li $v0, 1
+move $a0, $t2
+syscall			#Prints the Maximum Number
+li $v0, 10
+syscall
