7de79ba127d4be0d9540bbb6d4f934b443785a4f
Motawala <pkaran1100@gmail.com>
2023-03-23 13:03:57 -0700

    Radix Half count and print Data

--- newprog.s
+++ newprog.s
@@ -16,11 +16,15 @@ main:
 	
 	add $a0, $zero, $s0		#Pass in the array as a0 to the getMax function
 	add $a1, $zero, $s1		#Pass in the size of the array a1 to the getMax function
-	jal getMax
+	
+	jal radixSort
+	
+	addi $v0, $zero, 10
+	syscall
 	
 	
 getMax:
-	addi $sp, $sp, -16	#Allocates the memory inside the stack
+	addi $sp, $sp, -20	#Allocates the memory inside the stack
 	sw $ra, 0($sp)
 	sw $t0, 4($sp)
 	sw $t1, 8($sp)
@@ -36,30 +40,88 @@ getMax:
 	
 getMaxLoop:
 	beq $t1, $t2, exitMax		#if the size of the array is equal to i exit Max
+	addi $t0, $t0, 4
 	lw $t4, ($t0)				#Loads the second element of the array in t4
 	bge $t3, $t4, greater		#if t3 is greater than t4, t5=0
 	move $t3, $t4
+	add $v0, $zero, $t3			#Returns the max value to the caller function
 	
-	addi $t0, $t0, 4
-	jal getMaxLoop
-greater:
-	addi $t0, $t0, 4		#loads the next element of the Array
-	addi $t2, $t2, 1		#increment i by 1
-	jal getMaxLoop
+	
+	#addi $t2, $t2, 1		#increment i by 1
+	#j getMaxLoop
+	greater:
+		addi $t2, $t2, 1		#increment i by 1
+		bne $t2, $t1, getMaxLoop
 
 exitMax:
-	li $v0, 1
-	move $a0, $t3
-	syscall
+	
 	lw $ra, 0($sp)
 	lw $t0, 4($sp)
 	lw $t1, 8($sp)
 	lw $t2, 12($sp)
 	lw $t3, 16($sp)
 	
-	addi $sp, $sp, 16
+	addi $sp, $sp, 20
+	j $ra
+		
+	
+countSort:
+	move $t1, $a1
+	li $v0, 1
+	move $a0, $t1
+	syscall
 	j $ra
 	
+radixSort:
+	addi $sp, $sp, -20	#Allocates the memory inside the stack
+	sw $ra, 0($sp)
+	sw $t0, 4($sp)
+	sw $t1, 8($sp)
+	sw $t2, 12($sp)
+	sw $t3, 16($sp)
+	
+	add $a0, $zero, $s0		#Pass in the array as a0 to the getMax function
+	add $a1, $zero, $s1		#Pass in the size of the array a1 to the getMax function
+	jal getMax
+	
+	add $t0, $zero, $v0		#Loads the max value to a temp register t0
+	addi $t1, $zero, 1		#initialize exp to 1 in a2
+	jal radixSortLoop
+	
+	
+radixSortLoop:
+	
+	div $t0, $t1			#Divide the max by exp
+	mflo $t0				#Store the quotient in $t0
+	slt $t3, $zero, $t0
+	
+	
+	add $a0, $zero, $s0		#Pass in the argument array in Count Sort function
+	add $a1, $zero, $s1		#Pass in the argument size of the array in Count Sort function
+	add $a2, $zero, $t1		#Pass in the argument exp in the Count Sort function.
+	bne $t3, $zero, exitRadixSort		#if the (m/exp)<0 exit the function.
+	jal countSort			#Else call countSort function.
+	
+
+	addi $t4, $zero, 10		#adds 10 to the temp reg t4
+	mul $t1, $t1, $t4		#increment the exp by exp*=10
+	j radixSortLoop			#Loops back in to check the next condition.
+	
+	
+
+exitRadixSort:
+	lw $ra, 0($sp)
+	lw $t0, 4($sp)
+	lw $t1, 8($sp)
+	lw $t2, 12($sp)
+	lw $t3, 16($sp)
+	
+	addi $sp, $sp, 20
+	j $ra
+	jal end
+
+
+
 end:
 	addi $v0, $zero, 10
 	syscall
--- radix.asm
+++ radix.asm
@@ -6,29 +6,29 @@
 #$s0 = size
 #$t2 = max Number
 #$t1 = counter
-#$t4 = bool
+#$t4 = Quotient
 #$t5 = exp
 
 
 .data
-my_array: .word	1, 2, 3, 4, 8, 767
-size:	.word	5
+my_array: .word	1, 3, 4, 8, 767
+size:	.word	4
+msg:	.asciiz	"Max number "
 
 .text
 .globl main
 main:
-	la $t0, my_array	#loads the array into $t0
-	la $s0, size		#loads to the size of the array in $s0
-	#la $t4, msg
-	move $t1, $s0
-	lw $t2, 0($t0)		#loads the 1st element of an array
+	la $s0, my_array	#loads the array into $t0
+	la $s1, size		#loads to the size of the array in $s1
+	move $t1, $s1
+	lw $t2, 0($s0)		#loads the 1st element of an array
 	li $t5, 1			#initialize exp to 1
 	
 getMax:
 	#If the size of an array is 0 exit.
 	beq $t1, $zero, end
-	addi $t0, $t0, 4	#next element of the array
-	lw $t3, ($t0)		#load the current element
+	addi $s0, $s0, 4	#next element of the array
+	lw $t3, ($s0)		#load the current element
 	beq $t3, $zero, radixSort  # check if the end of the array is reached
 	bge $t2, $t3, greater
 	move $t2, $t3		#Updates the max
@@ -42,20 +42,22 @@ greater:
 
 radixSort:
 	div $t2, $t5	#divide the max number by exp and store it in max Number
-	mflo $t6
+	mflo $t4			#Loads the Quotient to $t4
 	mul $t5, $t5, 10	#Multipliying the exp by 10
 	jal loop
 	
 loop:
-	jal countSort
-	slt $t4, $zero, $t6
-	beq $t4, $zero, radixSort
+	jal countSort				#Calls countSort
+	bne $zero, $t4, radixSort	#Calls the RadixSort until M/Exp is < 0
 	jal end
 countSort:
+	li $v0, 4
+	la $a0, msg
+	syscall
 	li $v0, 1
 	move $a0, $t2
 	syscall
-	jr $ra
+	jr $ra						#Jumps back to caller function
 	
 #Exits the Program
 end:
